825f48e4e3edb55582f9339947db892d9a9f870d
biswajeetsamal1579 <biswajeetsamal1579@gmail.com>
2026-01-11 18:12:07 +0530

    updated a line

--- Increasing_triangle.java
+++ Increasing_triangle.java
@@ -8,6 +8,7 @@ class Increasing_triangle {
         System.out.println("enter the number of rows ");
         int n = sc.nextInt();
 
+        System.out.println("The pattern is : ");
         for(int i=1 ; i<=n ; i++){
 
             for(int j=1 ; j<=i ; j++){
